7b92f20325d012d38bfe0ddc248316c5e06b0947
Beim <beim2015@outlook.com>
2017-10-05 03:44:39 +0800

    '1'

--- conf/global_conf.js
+++ conf/global_conf.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    'env': 'dev', // 默认为开发环境，第一个命令行参数设置(app.js)
+    'env': process.argv[2] ? 'pro' : 'dev', // 默认为开发环境，第一个命令行参数设置(app.js)
     'port': 22001,
     'room_id': 221,
     'def_gift_list': ['辣条', '亿圆', '喵娘', '小电视', 'B坷垃', '自动铅笔', '神之记事本', '666', '233'],
